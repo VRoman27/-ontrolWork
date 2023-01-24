3cb163cea7a19977ab514f2c003aabf07cb7deb7
VRoman27 <romavolodin1@rambler.ru>
2023-01-24 21:16:21 +0300

    feat: add enter input array

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿
+string[] inputArray = InputStringArray();
+
+
+string[] InputStringArray()
+{
+    int length = IntData("Введите количество элементов: ");
+    string[] array = new string[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = InputString($"Введите элемент №{i}: ");
+    }
+
+    return array;
+}
+
+string InputString(string text)
+{
+    Console.Write(text);
+    string str = Console.ReadLine();
+    return str;
+}
+
+int IntData(string text)
+{
+    bool isParsed = false;
+    int number = 0;
+
+    while (!isParsed)
+    {
+        Console.Write(text);
+        isParsed = int.TryParse(Console.ReadLine(), out number);
+    }
+    return number;
+}
